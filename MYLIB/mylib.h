a32575c1e99348b7b9aa0453a8b03499f33bd126
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2024-04-24 13:01:14 +0100

    CHECK POINT <UPDATE MYLIB> ...

--- MYLIB/mylib.h
+++ MYLIB/mylib.h
@@ -25,7 +25,7 @@ int		ft_isdigit(int c);
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
-size_t	ft_strlen(const char *s);
+int		ft_strlen(const char *s);
 void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
@@ -87,7 +87,7 @@ int		ft_printf(const char *format, ...);
 /* GNL PROTOTYPES */
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 1
+#  define BUFFER_SIZE 1337
 # endif
 
 # define END '\0'
