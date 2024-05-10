d28a32fe1a6fdc7882adde99c6cc4805e827add7
ABDE-LKADER <abadouab@student.1337.ma>
2024-05-10 11:24:30 +0100

    CHECK POINT <-M-> <Done> ...

--- mandatory/mlx_sprites.c
+++ mandatory/mlx_sprites.c
@@ -6,7 +6,7 @@
 /*   By: abadouab <abadouab@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/24 15:54:07 by abadouab          #+#    #+#             */
-/*   Updated: 2024/05/09 21:30:40 by abadouab         ###   ########.fr       */
+/*   Updated: 2024/05/10 11:23:49 by abadouab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ static void	mlx_exit_effects(t_data *data, int count)
 	static int	index = E1;
 
 	(TRUE) && (px = data->exit_x * DM, py = data->exit_y * DM);
-	if (count % 299 == 0 && index < E8 && !data->map.collect)
+	if (count % 1000 == 0 && index < E8 && !data->map.collect)
 	{
 		mlx_put_img(data, GROUND, px, py);
 		mlx_put_image_to_window(data->mlx, data->win, data->img[E8], px, py);
@@ -34,7 +34,7 @@ static void	mlx_idle_effects(t_data *data, int count)
 	static int	index = I1;
 
 	(TRUE) && (px = data->pos_x * DM, py = data->pos_y * DM);
-	if (count % 299 == 0 && index <= I8 && !data->right && !data->up
+	if (count % 1000 == 0 && index <= I8 && !data->right && !data->up
 		&& !data->left && !data->down)
 	{
 		mlx_put_img(data, GROUND, px, py);
@@ -70,6 +70,36 @@ static void	mlx_fire_effects(t_data *data, t_map *map, int count)
 	}
 }
 
+////////////////////////// GRAVITY ////////////////////////////
+// static void	mlx_move_down(t_data *data)
+// {
+// 	int		px;
+// 	int		py;
+// 	int		move;
+// 	int		index;
+// 	int		count;
+
+// 	(TRUE) && (move = 7, index = A1,
+// 		px = data->pos_x * DM, py = data->pos_y * DM);
+// 	while (move <= 49)
+// 	{
+// 		mlx_put_img(data, GROUND, px, py);
+// 		mlx_put_img(data, GROUND, px, py + move);
+// 		mlx_put_image_to_window(data->mlx, data->win,
+// 			data->img[index++], px, py + move);
+// 		mlx_do_sync(data->mlx);
+// 		(index == A2) && (index = A1);
+// 		(move == 49) && (++data->pos_y);
+// 		if (data->map.map[data->pos_y][data->pos_x] == 'C')
+// 			data->map.collect--;
+// 		(move == 49) && (data->map.map[data->pos_y][data->pos_x] = 'P');
+// 		(move == 49) && (ft_printf(GRN"moves: %d\n"RST, data->steps++));
+// 		if (move == 49)
+// 			mlx_draw_string(data);
+// 		move += 7;
+// 	}
+// }
+
 int	mlx_do_effects(void *param)
 {
 	t_map		*map;
@@ -80,5 +110,8 @@ int	mlx_do_effects(void *param)
 	mlx_idle_effects(data, count);
 	mlx_exit_effects(data, count);
 	mlx_fire_effects(data, map, count);
+	// if (!ft_strchr("1E", map->map[data->pos_y + 1][data->pos_x])
+	// 	&& count % 100 == 0)
+	// 	mlx_move_down(data);
 	return (count++);
 }
--- mandatory/moves.c
+++ mandatory/moves.c
@@ -6,7 +6,7 @@
 /*   By: abadouab <abadouab@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 15:13:26 by abadouab          #+#    #+#             */
-/*   Updated: 2024/05/09 21:31:07 by abadouab         ###   ########.fr       */
+/*   Updated: 2024/05/10 11:16:48 by abadouab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,21 +20,22 @@ static void	mlx_move_up(t_data *data)
 	int		index;
 	int		count;
 
-	(TRUE) && (move = 7, index = R1,
+	(TRUE) && (move = 7, index = A1,
 		px = data->pos_x * DM, py = data->pos_y * DM);
-	while (index <= R8)
+	while (move <= 49)
 	{
 		mlx_put_img(data, GROUND, px, py);
 		mlx_put_img(data, GROUND, px, py - move);
 		mlx_put_image_to_window(data->mlx, data->win,
 			data->img[index++], px, py - move);
 		mlx_do_sync(data->mlx);
-		(index == R8) && (--data->pos_y);
+		(index == A2) && (index = A1);
+		(move == 49) && (--data->pos_y);
 		if (data->map.map[data->pos_y][data->pos_x] == 'C')
 			data->map.collect--;
-		(index == R8) && (data->map.map[data->pos_y][data->pos_x] = 'P');
-		(index == R8) && (ft_printf(GRN"moves: %d\n"RST, data->steps++));
-		if (index == R8)
+		(move == 49) && (data->map.map[data->pos_y][data->pos_x] = 'P');
+		(move == 49) && (ft_printf(GRN"moves: %d\n"RST, data->steps++));
+		if (move == 49)
 			mlx_draw_string(data);
 		move += 7;
 	}
@@ -48,21 +49,22 @@ static void	mlx_move_down(t_data *data)
 	int		index;
 	int		count;
 
-	(TRUE) && (move = 7, index = R1,
+	(TRUE) && (move = 7, index = A1,
 		px = data->pos_x * DM, py = data->pos_y * DM);
-	while (index <= R8)
+	while (move <= 49)
 	{
 		mlx_put_img(data, GROUND, px, py);
 		mlx_put_img(data, GROUND, px, py + move);
 		mlx_put_image_to_window(data->mlx, data->win,
 			data->img[index++], px, py + move);
 		mlx_do_sync(data->mlx);
-		(index == R8) && (++data->pos_y);
+		(index == A2) && (index = A1);
+		(move == 49) && (++data->pos_y);
 		if (data->map.map[data->pos_y][data->pos_x] == 'C')
 			data->map.collect--;
-		(index == R8) && (data->map.map[data->pos_y][data->pos_x] = 'P');
-		(index == R8) && (ft_printf(GRN"moves: %d\n"RST, data->steps++));
-		if (index == R8)
+		(move == 49) && (data->map.map[data->pos_y][data->pos_x] = 'P');
+		(move == 49) && (ft_printf(GRN"moves: %d\n"RST, data->steps++));
+		if (move == 49)
 			mlx_draw_string(data);
 		move += 7;
 	}
